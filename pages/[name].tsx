c59a55f4efed52bc687a1d12ea4444e8a41caf86
mostafa <mostafa.samir@thewebops.com>
2022-08-24 00:40:56 +0200

    Start in Draw Line Algo

--- pages/[name].tsx
+++ pages/[name].tsx
@@ -7,23 +7,65 @@ import { useEffect } from 'react';
 import Data from "../data/Data"
 import { ILine } from '../shared/Interfaces/line';
 
+const underLineHeight = 10;
+const spaceBetweenLine = 10;
+
 const Command: FC = () => {
     const router = useRouter();
     const codeLineRef = useRef<any>([]);
 
     const [commandData, setCommandData] = useState<ILine>();
 
-    const DrawLine = () => {
-        console.log(window.scrollY)
-        var c = document.getElementById("myCanvas") as HTMLCanvasElement | null;
+    const getBoardReady = () => {
+        let c = document.getElementById("drawBoard") as HTMLCanvasElement | null;
         c!.width = document.body.offsetWidth;
         c!.height = document.body.offsetHeight;
-        var ctx = c?.getContext("2d");
+        let ctx = c?.getContext("2d");
         ctx?.beginPath();
-        const startX = codeLineRef.current[0].getBoundingClientRect().left  + (codeLineRef.current[0].getBoundingClientRect().width / 2)
-        const startY = codeLineRef.current[0].getBoundingClientRect().top + codeLineRef.current[0].getBoundingClientRect().height
-        ctx?.moveTo(startX, startY);
-        ctx?.lineTo(startX, startY + 50);
+        return ctx;
+    }
+
+    const getcodeLinePrefrence = (item: any) => {
+        const top = item.getBoundingClientRect().top;
+        const left = item.getBoundingClientRect().left;
+        const width = item.getBoundingClientRect().width;
+        const height = item.getBoundingClientRect().height;
+
+        return { top, left, width, height }
+    }
+
+    const drawUnderLine = (ctx: any) => {
+        codeLineRef.current.map((item: any) => {
+            const { top, left, width, height } = getcodeLinePrefrence(item);
+
+            const startX = left;
+            const startY = top + height;
+
+            ctx?.moveTo(startX, startY);
+            ctx?.lineTo(startX, startY + underLineHeight);
+            ctx?.lineTo(startX + width, startY + underLineHeight);
+            ctx?.lineTo(startX + width, startY);
+        })
+        return ctx;
+    }
+
+    const drawFlowLine = (ctx: any) => {
+        codeLineRef.current.map((item: any, index: number) => {
+            const { top, left, width, height } = getcodeLinePrefrence(item);
+
+            const startX = left + (width / 2);
+            const startY = top + height;
+
+            ctx?.moveTo(startX, startY + underLineHeight);
+            ctx?.lineTo(startX, startY + underLineHeight + 10 + (index * spaceBetweenLine));
+        })
+        return ctx;
+    }
+
+    const drawLine = () => {
+        let ctx = getBoardReady();
+        ctx = drawUnderLine(ctx);
+        ctx = drawFlowLine(ctx);
         ctx?.stroke();
     }
 
@@ -35,12 +77,12 @@ const Command: FC = () => {
     }, [router])
 
     useEffect(() => {
-        if(commandData && document) DrawLine();
+        if(commandData && document) drawLine();
     }, [commandData])
 
     return (
         <>
-        <canvas id="myCanvas" style={{position:"absolute", border :"1px solid #d3d3d3;", top: "0", left: "0"}}>
+        <canvas id="drawBoard" style={{position:"absolute", border :"1px solid #d3d3d3", top: "0", left: "0"}}>
             Your browser does not support the HTML5 canvas tag.</canvas>
             <Header />
             {commandData && <CodeLine codeLineRef={codeLineRef} commandData={commandData} />}
